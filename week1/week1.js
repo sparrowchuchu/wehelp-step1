d7e546d12939ee619fed0a8bd4b9b3767ae5ab0b
Zospi <a9000102030@gmail.com>
2025-01-12 23:43:18 +0800

    week1

---
+++ week1/week1.js
@@ -0,0 +1,16 @@
+
+const dropOption = document.querySelector('.dropOption');
+const dropdown = document.querySelector('.dropdown');
+const btnX = document.querySelector('.btn-x');
+
+const toggleDropdown = () =>{
+    dropOption.style.display = "none";
+    dropdown.style.display = "block";
+}
+const toggleClose = () =>{
+    dropOption.style.display = "block";
+    dropdown.style.display = "none";
+}
+
+dropOption.addEventListener('click', toggleDropdown);
+btnX.addEventListener('click', toggleClose);

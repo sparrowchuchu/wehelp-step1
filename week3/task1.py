97c796f96ed53bf585864ab2dc2812235b426b82
Zospi <a9000102030@gmail.com>
2025-01-22 19:27:23 +0800

    task1 & task2

--- week3/task1.py
+++ week3/task1.py
@@ -12,8 +12,6 @@ def getUrlData(url):
         response = request.urlopen(req)
     except error.HTTPError as e:
         print(e)
-    else:
-        print("Get Url Data Successfully.")
     data = response.read().decode('utf-8')
     dict_data = json.loads(data)
     return dict_data
@@ -32,15 +30,11 @@ urls = {
     'ass_2':'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2'
     }
 
-'''
-1. 取得資料
-'''
+# 1. 取得資料
 ass_1 = getUrlData(urls['ass_1'])
 ass_2 = getUrlData(urls['ass_2'])
 
-'''
-2. 檢查資料/清理資料
-'''
+# 2. 檢查資料/清理資料
 list_ass_1 = ass_1['data']['results']
 result_ass_1 = []  # ['SERIAL_NO', 'stitle', 'longitude', 'latitude', 'info']
 for dict_data in list_ass_1:
@@ -49,7 +43,6 @@ for dict_data in list_ass_1:
     match = re.search(pattern, dict_data['filelist'], re.IGNORECASE)  # re.I 搜尋忽略大小寫
     if match:
         img_url = match.group()
-        # print(dict_data['_id'], "找到的匹配 Image URL \n", img_url)
     else:
         print(dict_data['_id'], "String 內不含符合規則的 Image URL")
     result_ass_1.append([dict_data['SERIAL_NO'], dict_data['stitle'], dict_data['longitude'],
@@ -60,22 +53,18 @@ list_2_ass_2 = [list(item.values()) for item in list_ass_2]
 result_ass_2 = []  # ['SERIAL_NO', 'MRT', district]
 set_mrt = set()
 for item in list_2_ass_2:
-    # print(item)
     pattern = r'[\u4e00-\u9fff]{2}區'  # 所有中文字 [\u4E00-\u9FFF]
     match = re.search(pattern, item[2], re.IGNORECASE)  # re.I 搜尋忽略大小寫
     if match:
         district = match.group()
-        # print("District :", district)
     else:
         print(item[2], "String 內不含符合規則的 District")
     set_mrt.add(item[0])
     result_ass_2.append([item[1], item[0], district])
-writeCsvFile('ass_1.csv', result_ass_1)
-writeCsvFile('ass_2.csv', result_ass_2)
+# writeCsvFile('ass_1.csv', result_ass_1)
+# writeCsvFile('ass_2.csv', result_ass_2)
 
-'''
-3. 整理資料/輸出資料
-'''
+# 3. 整理資料/輸出資料
 spot = []  # [SpotTitle, District, Longitude, Latitude, ImageURL]
 mrt = [[i] for i in set_mrt]  # [StationName, AttractionTitle1, AttractionTitle2, AttractionTitle3, ...]
 for i in result_ass_1:  # ['SERIAL_NO', 'stitle', 'longitude', 'latitude', 'info']
@@ -83,8 +72,7 @@ for i in result_ass_1:  # ['SERIAL_NO', 'stitle', 'longitude', 'latitude', 'info
         if i[0] == j[0]:
             spot.append([i[1], j[2], i[2], i[3] ,i[4]])
             for m in mrt:
-                if j[1] == m[0]:
-                    print(j[2], m[0])
+                if j[1] == m[0]:  # MRT == StationName
                     m.append(i[1])
                     break
             break
--- week3/task2.py
+++ week3/task2.py
@@ -2,91 +2,103 @@ from urllib import request, error
 from http import cookiejar
 from bs4 import BeautifulSoup
 import csv
-import re
+
 
 def getUrlData(url):
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 \
-           Safari/537.36'}
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 \
+            Safari/537.36'}
+    cj = cookiejar.CookieJar()
+    # Manually create a cookie and add it to the CookieJar
+    cookie = cookiejar.Cookie(
+        version=0,  # Cookie version (0 means Netscape format)
+        name='over18',
+        value='1',
+        port=None,
+        port_specified=False,
+        domain='www.ptt.cc',
+        domain_specified=True,
+        domain_initial_dot=False,
+        path='/',  # Path of the cookie
+        path_specified=True,
+        secure=False,  # Whether the cookie is secure
+        expires=None,  # Expires (None means session cookie)
+        discard=True,
+        comment=None,
+        comment_url=None,
+        rest={}
+    )
+    cj.set_cookie(cookie)
+    opener = request.build_opener(request.HTTPCookieProcessor(cj))
     try:
         req = request.Request(url, headers = headers)
-        response = request.urlopen(req)
+        response = opener.open(req)
     except error.HTTPError as e:
-        print(e)
+        # print(e)
+        pass
     else:
-        print("Get Url Data Successfully.")
+        pass
     data = response.read().decode('utf-8')
     soup = BeautifulSoup(data, 'html.parser')
-    # print(soup.prettify())
-    print("Parse Url Data Successfully.") 
+    # print(soup.prettify())    
     return soup
 
+def writeCsvFile(fileName, result):
+    with open(fileName, 'w', encoding='utf-8', newline = '') as csvFile:
+        csvWriter = csv.writer(csvFile)
+        for row in result:
+            csvWriter.writerow(row)
+    print(f'Write CSV File {fileName} Successfully.')
+    
+
+article_title = []
+like_count = []
+publish_time = []
+
+# 1. 取得資料/清理資料
 # Here is URL for PTT lottery board:
-url = 'https://www.ptt.cc/bbs/Lottery/index.html'
+ppt_url = 'https://www.ptt.cc/bbs/Lottery/index.html'
+soup = getUrlData(ppt_url)
 
+page = 1
+while page < 4: 
+    print(f'start getting page {page} data.')
+    titles = soup('div', 'title')
+    for title in titles:
+        try:
+            article_title.append(title.a.text)
+        except Exception as e:  # 本文已被刪除
+            article_title.append(title.text.strip())
+            publish_time.append('')
+            # print(e)
+        else:
+            url = 'https://www.ptt.cc' + title.a['href']
+            try:
+                article = getUrlData(url)
+                article_metaline = article('div', 'article-metaline') [2]
+                pt = article_metaline('span', 'article-meta-value')[0].text
+                publish_time.append(pt)
+            except Exception as e:    # [公告] 沒有發布日期
+                publish_time.append('')
+                # print(e)
+    
+    likes = soup('div', 'nrec')
+    for like in likes:
+        try:
+            like_count.append(like.span.text)
+        except Exception as e:  # 沒有讚或噓
+            like_count.append('0')
+            # print(e)
+    
+    next_page = soup('a', 'btn wide')[1]['href']
+    next_page_url = 'https://www.ptt.cc' + next_page
+    soup = getUrlData(next_page_url)
+    page += 1
 
-'''
-1. 取得資料/解析資料
-'''
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 \
-        Safari/537.36'}
-cj = cookiejar.CookieJar()
+# 2. 整理資料
+lottery = [[article_title[i], like_count[i], publish_time[i]] for i in range(len(article_title))]
 
-# Manually create a cookie and add it to the CookieJar
-cookie = cookiejar.Cookie(
-    version=0,  # Cookie version (0 means Netscape format)
-    name='over18',
-    value='1',
-    port=None,
-    port_specified=False,
-    domain='www.ptt.cc',  # Replace with the actual domain you're requesting
-    domain_specified=True,
-    domain_initial_dot=False,
-    path='/',  # Path of the cookie
-    path_specified=True,
-    secure=False,  # Whether the cookie is secure
-    expires=None,  # Expires (None means session cookie)
-    discard=True,
-    comment=None,
-    comment_url=None,
-    rest={}
-)
-cj.set_cookie(cookie)
-opener = request.build_opener(request.HTTPCookieProcessor(cj))
-try:
-    req = request.Request(url, headers = headers)
-    response = opener.open(req)
-except error.HTTPError as e:
-    print(e)
-else:
-    print("Get Url Data Successfully.")
-data = response.read().decode('utf-8')
-soup = BeautifulSoup(data, 'html.parser')
-# print(soup.prettify())
-print("Parse Url Data Successfully.")  
-
-
-'''
-2. 爬取資料/清理資料
-'''
-article = []
-titles = soup("div", "title")
-nrecs = soup("div", "nrec")
-like = []
-for nrec in nrecs:
-    for i in nrec:
-        # print(i.contents)
-        like.append(i.contents[0])
-        
-print(like)
-print(titles)
-
-
-
-'''
-3. 整理資料/輸出資料
-'''
-
+# 3. 輸出資料
+writeCsvFile('article.csv', lottery)
 
 
